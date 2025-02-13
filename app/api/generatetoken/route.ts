868529834d27cd6ebc7c7d2c32dd1bbd419cfaef
Meghanareddy244 <meghanareddys999@gmail.com>
2025-02-13 14:17:08 +0530

    inital commit

---
+++ app/api/generatetoken/route.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
+
+// Define the type for the response body
+type Data = {
+  token?: string;
+  error?: string;
+};
+
+// The handler function for the API route
+export async function GET() {
+  // Get API key and secret from environment variables
+  const apiKey = process.env.API_KEY;
+  const apiSecret = process.env.API_SECRET;
+
+  // Validate API key and secret
+  if (!apiKey || !apiSecret) {
+    return new NextResponse("API key or secret not configured in environment variables",{status:500})
+  }
+
+  try {
+    // Generate JWT
+    const token = jwt.sign(
+      {
+        iss: apiKey,
+        exp: Math.floor(Date.now() / 1000) + 3600, // Token expires in 1 hour
+      },
+      apiSecret
+    );
+
+    // Return the token
+    return NextResponse.json({token: `Bearer ${token}`})
+  } catch (error) {
+    console.error("Error generating token:", error);
+    return new NextResponse("Internal Server Error",{status:500})
+  }
+}
